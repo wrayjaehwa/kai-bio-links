3d16bba8122e039db67c4732de1c23355325b0ed
wrayjaehwa <jaehwawray@gmail.com>
2024-01-08 19:23:59 -0500

    initial

---
+++ script.js
@@ -0,0 +1,15 @@
+function toggleMode() {
+  const html = document.documentElement;
+
+  html.classList.toggle('light');
+
+  const img = document.querySelector('#profile img');
+
+  if (html.classList.contains('light')) {
+    img.setAttribute('src', './assets/avatar-light.png');
+    img.setAttribute('alt', 'Light theme profile picture');
+  } else {
+    img.setAttribute('src', './assets/avatar-dark.png');
+    img.setAttribute('alt', 'Dark theme profile picture');
+  }
+}
